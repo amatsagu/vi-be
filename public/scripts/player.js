3fdab7eaa3c860ebcba74b95323d10967cf7ed3a
Amatsagu <krzysztof.rapior@protonmail.com>
2022-03-30 01:57:58 +0200

    Start working at Player monolith

---
+++ public/scripts/player.js
@@ -0,0 +1,58 @@
+class Player {
+    static #isPlaying = false;
+    static #volume = parseFloat(localStorage.getItem("volume") ?? "0.5");
+    static #playlistUrl = localStorage.getItem("playlistUrl") ?? "https://raw.githubusercontent.com/Amatsagu/Vi-be/master/playlist";
+    static #playback = localStorage.getItem("playback") ?? undefined;
+    static #tracks = [];
+    static #lastPlayedIds = [];
+    static #loopLimit = 5; // How many songs should Player remember back to not repeat them. Take 20% of total playlist length.
+    static #speaker = (() => {
+        const audio = new Audio;
+        audio.crossOrigin = "anonymous";
+        audio.volume = Player.#volume;
+        audio.muted = true;
+        return audio;
+    })();
+
+    static async _init() {
+        await Player.setPlaylist(Player.#playlistUrl);
+    }
+
+    static async setPlaylist(url, silent) {
+        if (!silent) console.log(`[Player] [Info] Verifying ${url} as a new playlist source. Fetching "details.json" file from ${url}/details.json uri.`);
+
+        let res;
+        try {
+            res = await (await fetch(`${url}/details.json`)).json();
+        }
+        catch {
+            if (!silent) console.log("[Player] [Error] Failed to fetch. Provided playlist url is considered as invalid.");
+        }
+
+        // Check
+        let idx = 0;
+        for (const { id, title, author, duration } of res) {
+            if (typeof id != "string" || typeof title != "string" || (author && typeof author != "string") || typeof duration != "number") {
+                if (!silent) console.log(`[Player] [Error] Failed to fetch. Track[${idx}] doesn't meet schema.`);
+                return;
+            }
+            idx++;
+        }
+
+        localStorage.setItem("playlistUrl", url);
+        Player.#playlistUrl = url;
+        Player.#tracks = res;
+        Player.#loopLimit = Math.round(res.length * 0.2);
+        if (!silent) console.log(`[Player] [Info] Provided playlist url \"details.json\" file got accepted (tracks = ${Player.#tracks.length}, loopLimit = ${Player.#loopLimit}). Keep in mind it only checks data file, it won't check every single audio source.`);
+    }
+
+    static play() {
+        if (Player.#isPlaying) return console.log("[Player] [Warn] Stream is already playing!");
+    }
+
+    static pause() {
+        if (!Player.#isPlaying) return console.log("[Player] [Warn] Stream is already paused!");
+    }
+}
+
+Player._init();
