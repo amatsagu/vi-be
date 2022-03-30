4be4a493b43eed03d13f3810a0f8529607df73ca
Amatsagu <krzysztof.rapior@protonmail.com>
2022-03-30 17:01:55 +0200

    Working player & visualizer

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -1,23 +1,60 @@
 class Player {
     static #isPlaying = false;
+    static #linkedVisualizer = false;
     static #volume = parseFloat(localStorage.getItem("volume") ?? "0.5");
     static #playlistUrl = localStorage.getItem("playlistUrl") ?? "https://raw.githubusercontent.com/Amatsagu/Vi-be/master/playlist";
-    static #playback = localStorage.getItem("playback") ?? undefined;
+    static #playback = localStorage.getItem("playback") && JSON.parse(localStorage.getItem("playback"));
     static #tracks = [];
     static #lastPlayedIds = [];
     static #loopLimit = 5; // How many songs should Player remember back to not repeat them. Take 20% of total playlist length.
     static #switch = document.getElementById("switch-btn");
     static #speaker = (() => {
-        const audio = new Audio;
+        const audio = new Audio();
         audio.crossOrigin = "anonymous";
         audio.volume = Player.#volume;
-        audio.muted = true;
         return audio;
     })();
 
     static async _init() {
-        await Player.setPlaylist(Player.#playlistUrl);
+        await Player.setPlaylist(Player.#playlistUrl, true);
         Player.#switch.onclick = Player.play;
+        Visualizer._init();
+    }
+
+    static #getTrack() {
+        const now = Date.now();
+        if (Player.#playback && now < Player.#playback.endTimestamp) {
+            const track = Player.#tracks.find((track) => track.id == Player.#playback.trackId);
+            if (!track) {
+                localStorage.removeItem("playback");
+                Player.#playback = undefined;
+                return Player.#getTrack();
+            }
+
+            Player.#lastPlayedIds.push(track.id);
+            return { ...track, offset: track.duration - (Player.#playback.endTimestamp - now) };
+        }
+
+        while (true) {
+            const track = Player.#tracks[getRandomInt(0, Player.#tracks.length)];
+            if (Player.#lastPlayedIds.includes(track.id)) continue;
+
+            Player.#playback = { trackId: track.id, endTimestamp: now + track.duration };
+            Player.#lastPlayedIds.push(track.id);
+            localStorage.setItem("playback", JSON.stringify(Player.#playback));
+            return { ...track, offset: 0 };
+        }
+    }
+
+    static #setTrackDetails(title, author) {
+        const titleNode = document.getElementById("track-title");
+        const authorNode = document.getElementById("track-author");
+
+        title = title.replace(/\(.+\)/gi, "").substr(0, 24);
+
+        titleNode.innerText = title;
+        authorNode.innerText = (author && author.substr(0, 32)) ?? "";
+        document.title = `${title} - Vi~be`;
     }
 
     static async setPlaylist(url, silent) {
@@ -26,8 +63,7 @@ class Player {
         let res;
         try {
             res = await (await fetch(`${url}/details.json`)).json();
-        }
-        catch {
+        } catch {
             if (!silent) console.log("[Player] [Error] Failed to fetch. Provided playlist url is considered as invalid.");
         }
 
@@ -48,11 +84,33 @@ class Player {
         if (!silent) console.log(`[Player] [Info] Provided playlist url \"details.json\" file got accepted (tracks = ${Player.#tracks.length}, loopLimit = ${Player.#loopLimit}). Keep in mind it only checks data file, it won't check every single audio source.`);
     }
 
-    static play() {
+    static async play() {
         if (Player.#isPlaying) return console.log("[Player] [Warn] Stream is already playing!");
         Player.#switch.style.fill = "rgb(64, 180, 128)";
         Player.#switch.onclick = Player.pause;
-        Player.#isPlaying = true;
+
+        if (!Player.#linkedVisualizer) {
+            Player.#linkedVisualizer = true;
+            Visualizer._link(Player.#speaker);
+        }
+
+        const res = Player.#getTrack();
+        Player.#setTrackDetails(res.title, res.author);
+        Player.#speaker.src = `${Player.#playlistUrl}/tracks/${res.id}.ogg`;
+
+        Player.#speaker.oncanplay = () => {
+            Player.#speaker.oncanplay = null;
+            Player.#speaker.currentTime = Math.floor(res.offset / 1000);
+            if (Player.#speaker.paused) Player.#speaker.play();
+            Player.#isPlaying = true;
+            requestAnimationFrame(Visualizer.render);
+        };
+
+        Player.#speaker.onended = () => {
+            Player.#speaker.onended = null;
+            Player.#isPlaying = false;
+            Player.play();
+        }
     }
 
     static pause() {
@@ -60,7 +118,6 @@ class Player {
         Player.#switch.style.fill = "#e2e2e2";
         Player.#switch.onclick = Player.play;
         Player.#isPlaying = false;
+        Player.#speaker.pause();
     }
 }
-
-Player._init();
--- public/scripts/visualizer.js
+++ public/scripts/visualizer.js
@@ -1,90 +1,98 @@
-"use strict";
+class Visualizer {
+    static #RADIUS = 256;
+    static #BAR_COUNT = 180;
+    static #SCALE = 0.5;
+    static #canvas = document.getElementById("visualizer");
+    static #ctx = Visualizer.#canvas.getContext("2d");
+    static #audioContext;
+    static #analyser;
+    static #analyserSource;
+    static #freqs;
+    static baseColor = "rgb(128, 128, 128)";
 
-const RADIUS = 256;
-const BAR_COUNT = 180;
-
-/** @returns {MediaStream} */
-async function getStream(url) {
-    let res;
-    const mutex = new Promise((resolve) => (res = resolve));
-
-    const speaker = new Audio();
-    speaker.crossOrigin = "anonymous";
-    speaker.src = url;
-    speaker.oncanplay = () => res();
-
-    await mutex;
-
-    speaker.play();
-    return speaker;
-}
-
-function resizeCanvas() {
-    const canvas = document.getElementById("visualizer");
-    const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "rgb(128, 128, 128)";
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, RADIUS, 0, 2 * Math.PI);
-    ctx.stroke();
-}
-
-async function play() {
-    /** @type {AudioContext} */
-    const context = new (window.AudioContext || window.webkitAudioContext)();
-    const analyser = context.createAnalyser();
-
-    const stream = await getStream("https://raw.githubusercontent.com/Amatsagu/Yaikava/master/dist/tracks/T_Gux-qXQV8.ogg");
-    stream.volume = 0.3;
-    const source = context.createMediaElementSource(stream);
-
-    source.connect(analyser);
-    analyser.connect(context.destination);
-
-    resizeCanvas();
-
-    const canvas = document.getElementById("visualizer");
-    const ctx = canvas.getContext("2d");
-    ctx.imageSmoothingEnabled = true;
-    const freqs = new Uint8Array(analyser.frequencyBinCount);
-
-    function render() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeStyle = "rgb(64, 180, 128)";
-        ctx.beginPath();
-        ctx.arc(canvas.width / 2, canvas.height / 2, RADIUS, 0, 2 * Math.PI);
-        ctx.stroke();
-        analyser.getByteFrequencyData(freqs);
-
-        for (var i = 0; i < BAR_COUNT; i++) {
-            const radians = (Math.PI * 2) / BAR_COUNT;
-            const bar_height = freqs[i] * 0.5;
-
-            const x = canvas.width / 2 + Math.cos(radians * i) * RADIUS;
-            const y = canvas.height / 2 + Math.sin(radians * i) * RADIUS;
-            const limitX = canvas.width / 2 + Math.cos(radians * i) * (RADIUS + bar_height);
-            const limitY = canvas.height / 2 + Math.sin(radians * i) * (RADIUS + bar_height);
-            ctx.strokeStyle = `rgb(64, ${freqs[i] + 48}, 128)`;
-            ctx.lineWidth = 3;
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(limitX, limitY);
-            ctx.stroke();
-        }
-
-        requestAnimationFrame(render);
+    static _init() {
+        Visualizer.#ctx.imageSmoothingEnabled = true;
+        window.onresize = () => Visualizer.update();
+        Visualizer.update();
     }
 
-    requestAnimationFrame(render);
-    window.onclick = null;
-}
+    static _link(speaker) {
+        Visualizer.#audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        Visualizer.#analyser = Visualizer.#audioContext.createAnalyser();
+        Visualizer.#analyserSource = Visualizer.#audioContext.createMediaElementSource(speaker);
+        Visualizer.#analyserSource.connect(Visualizer.#analyser);
+        Visualizer.#analyser.connect(Visualizer.#audioContext.destination);
+        Visualizer.#freqs = new Uint8Array(Visualizer.#analyser.frequencyBinCount);
+    }
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-window.onload = () => resizeCanvas();
-window.onclick = () => play();
-window.onresize = () => resizeCanvas();
+    static update() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        // Manually try to scale canvas cause CSS commonly fails.
+        if (width > 780) {
+            Visualizer.#RADIUS = 256;
+            Visualizer.#BAR_COUNT = 180;
+            Visualizer.#SCALE = 0.5;
+        }
+        else if (width <= 300) {
+            Visualizer.#RADIUS = 96;
+            Visualizer.#BAR_COUNT = 64;
+            Visualizer.#SCALE = 0.2;
+        }
+        else if (width <= 440) {
+            Visualizer.#RADIUS = 128;
+            Visualizer.#BAR_COUNT = 96;
+            Visualizer.#SCALE = 0.25;
+        }
+        else if (width <= 560) {
+            Visualizer.#SCALE = 0.3;
+        }
+        else if (width <= 780) {
+            Visualizer.#RADIUS = 192;
+            Visualizer.#BAR_COUNT = 128;
+            Visualizer.#SCALE = 0.4;
+        }
+
+        Visualizer.#canvas.width = width;
+        Visualizer.#canvas.height = height;
+        Visualizer.#ctx.canvas.width = width;
+        Visualizer.#ctx.canvas.height = height;
+        Visualizer.#ctx.clearRect(0, 0, width, height);
+        Visualizer.#ctx.strokeStyle = Visualizer.baseColor;
+        Visualizer.#ctx.lineWidth = 3;
+        Visualizer.#ctx.beginPath();
+        Visualizer.#ctx.arc(width / 2, height / 2, Visualizer.#RADIUS, 0, 2 * Math.PI);
+        Visualizer.#ctx.stroke();
+    }
+
+    static render() {
+        const width = Visualizer.#canvas.width;
+        const height = Visualizer.#canvas.height;
+
+        Visualizer.#ctx.clearRect(0, 0, width, height);
+        Visualizer.#ctx.strokeStyle = "rgb(64, 180, 128)";
+        Visualizer.#ctx.beginPath();
+        Visualizer.#ctx.arc(width / 2, height / 2, Visualizer.#RADIUS, 0, 2 * Math.PI);
+        Visualizer.#ctx.stroke();
+        Visualizer.#analyser.getByteFrequencyData(Visualizer.#freqs);
+
+        for (var i = 0; i < Visualizer.#BAR_COUNT; i++) {
+            const radians = (Math.PI * 2) / Visualizer.#BAR_COUNT;
+            const size = Visualizer.#freqs[i] * Visualizer.#SCALE;
+
+            const x = width / 2 + Math.cos(radians * i) * Visualizer.#RADIUS;
+            const y = height / 2 + Math.sin(radians * i) * Visualizer.#RADIUS;
+            const limitX = width / 2 + Math.cos(radians * i) * (Visualizer.#RADIUS + size);
+            const limitY = height / 2 + Math.sin(radians * i) * (Visualizer.#RADIUS + size);
+            Visualizer.#ctx.strokeStyle = `rgb(64, ${Visualizer.#freqs[i] + 48}, 128)`;
+            Visualizer.#ctx.lineWidth = 3;
+            Visualizer.#ctx.beginPath();
+            Visualizer.#ctx.moveTo(x, y);
+            Visualizer.#ctx.lineTo(limitX, limitY);
+            Visualizer.#ctx.stroke();
+        }
+
+        requestAnimationFrame(Visualizer.render);
+    }
+}
